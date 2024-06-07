4938c9bf8708385ae66ede1eba99aeb57ef16d64
Leandro Moreira <leandromoreira.dev@gmail.com>
2024-06-07 17:13:46 -0300

    Para deploy

--- basedados/bd.go
+++ basedados/bd.go
@@ -14,7 +14,8 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=webadmin password=7zPz28k6Bz dbname=gicoobj port=5432 sslmode=disable"
+	// stringDeConexao := "host=localhost user=webadmin password=7zPz28k6Bz dbname=gicoobj port=5432 sslmode=disable"
+	stringDeConexao := "host=localhost user=postgres password=postgres dbname=gicoobj port=5432 sslmode=disable"
 
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 
